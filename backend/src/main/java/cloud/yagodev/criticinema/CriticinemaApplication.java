9bf9df68b4f82837696c4dab204dabfee360677a
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-05-20 20:07:55 -0300

    Iniciando Projeto

---
+++ backend/src/main/java/cloud/yagodev/criticinema/CriticinemaApplication.java
@@ -0,0 +1,13 @@
+package cloud.yagodev.criticinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticinemaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cloud/yagodev/criticinema/CriticinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.yagodev.criticinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
